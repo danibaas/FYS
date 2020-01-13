eff55e25a66b7a96377f493276b5282435836601
Dani Baas <Dani.raj25@gmail.com>
2020-01-13 08:34:21 +0100

    ok

--- Main/assets/WorkbenchRaw.sql
+++ Main/assets/WorkbenchRaw.sql
@@ -5,155 +5,137 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema zbaasdr
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema zbaasdr
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `zbaasdr` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `zbaasdr` DEFAULT CHARACTER SET latin1 ;
 USE `zbaasdr` ;
 
 -- -----------------------------------------------------
 -- Table `zbaasdr`.`Account`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `zbaasdr`.`Account` (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(150) NOT NULL,
+  `user_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(150) NULL DEFAULT NULL,
+  `coins` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`user_id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `zbaasdr`.`Achievement`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `zbaasdr`.`Achievement` (
-  `achievementid` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(300) NOT NULL,
+  `achievementid` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(300) NULL DEFAULT NULL,
   PRIMARY KEY (`achievementid`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `zbaasdr`.`Achieved`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `zbaasdr`.`Achieved` (
-  `Achievement_achievementid` INT NOT NULL,
-  `Account_user_id` INT NOT NULL,
-  PRIMARY KEY (`Achievement_achievementid`, `Account_user_id`),
-  INDEX `fk_Achievement_has_Account_Account1_idx` (`Account_user_id` ASC) VISIBLE,
-  INDEX `fk_Achievement_has_Account_Achievement_idx` (`Achievement_achievementid` ASC) VISIBLE,
-  CONSTRAINT `fk_Achievement_has_Account_Achievement`
-    FOREIGN KEY (`Achievement_achievementid`)
-    REFERENCES `zbaasdr`.`Achievement` (`achievementid`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Achievement_has_Account_Account1`
-    FOREIGN KEY (`Account_user_id`)
-    REFERENCES `zbaasdr`.`Account` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `zbaasdr`.`Highscore`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `zbaasdr`.`Highscore` (
-  `score` FLOAT NOT NULL,
-  `Account_user_id` INT NOT NULL,
-  INDEX `fk_Highscore_Account1_idx` (`Account_user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Highscore_Account1`
-    FOREIGN KEY (`Account_user_id`)
-    REFERENCES `zbaasdr`.`Account` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `zbaasdr`.`Money`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `zbaasdr`.`Money` (
-  `coins` INT NOT NULL,
-  `Account_user_id` INT NOT NULL,
-  PRIMARY KEY (`Account_user_id`),
-  INDEX `fk_Money_Account1_idx` (`Account_user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Money_Account1`
-    FOREIGN KEY (`Account_user_id`)
-    REFERENCES `zbaasdr`.`Account` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `zbaasdr`.`Item`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `zbaasdr`.`Item` (
-  `itemid` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(200) NOT NULL,
-  PRIMARY KEY (`itemid`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `zbaasdr`.`Shop`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `zbaasdr`.`Shop` (
-  `Item_itemid` INT NOT NULL,
-  `Account_user_id` INT NOT NULL,
-  PRIMARY KEY (`Item_itemid`, `Account_user_id`),
-  INDEX `fk_Item_has_Account_Account1_idx` (`Account_user_id` ASC) VISIBLE,
-  INDEX `fk_Item_has_Account_Item1_idx` (`Item_itemid` ASC) VISIBLE,
-  CONSTRAINT `fk_Item_has_Account_Item1`
-    FOREIGN KEY (`Item_itemid`)
-    REFERENCES `zbaasdr`.`Item` (`itemid`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Item_has_Account_Account1`
-    FOREIGN KEY (`Account_user_id`)
-    REFERENCES `zbaasdr`.`Account` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  `user_id` INT(11) NOT NULL,
+  `achievementid` INT(11) NOT NULL,
+  PRIMARY KEY (`user_id`, `achievementid`),
+  INDEX `achievementid` (`achievementid` ASC) VISIBLE,
+  CONSTRAINT `Achieved_ibfk_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `zbaasdr`.`Account` (`user_id`),
+  CONSTRAINT `Achieved_ibfk_2`
+    FOREIGN KEY (`achievementid`)
+    REFERENCES `zbaasdr`.`Achievement` (`achievementid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `zbaasdr`.`Gamerun`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `zbaasdr`.`Gamerun` (
-  `startTime` INT NOT NULL,
-  `enemiesKilled` INT NOT NULL,
-  `bossesKilled` INT NOT NULL,
-  `score` INT NOT NULL,
-  `Account_user_id` INT NOT NULL,
-  PRIMARY KEY (`startTime`, `Account_user_id`),
-  INDEX `fk_Gamerun_Account1_idx` (`Account_user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Gamerun_Account1`
-    FOREIGN KEY (`Account_user_id`)
-    REFERENCES `zbaasdr`.`Account` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  `user_id` INT(11) NOT NULL,
+  `startTime` INT(11) NOT NULL,
+  `enemiesKilled` INT(11) NULL DEFAULT NULL,
+  `bossesKilled` INT(11) NULL DEFAULT NULL,
+  `score` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`, `startTime`),
+  CONSTRAINT `Gamerun_ibfk_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `zbaasdr`.`Account` (`user_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `zbaasdr`.`Highscore`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `zbaasdr`.`Highscore` (
+  `user_id` INT(11) NULL DEFAULT NULL,
+  `score` FLOAT NULL DEFAULT NULL,
+  INDEX `user_id` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `Highscore_ibfk_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `zbaasdr`.`Account` (`user_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `zbaasdr`.`Item`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `zbaasdr`.`Item` (
+  `itemid` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NULL DEFAULT NULL,
+  `value` INT(11) NULL DEFAULT NULL,
+  `level` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`itemid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `zbaasdr`.`Inventory`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `zbaasdr`.`Inventory` (
+  `user_id` INT(11) NOT NULL,
+  `itemid` INT(11) NOT NULL,
+  PRIMARY KEY (`user_id`, `itemid`),
+  INDEX `itemid` (`itemid` ASC) VISIBLE,
+  CONSTRAINT `Inventory_ibfk_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `zbaasdr`.`Account` (`user_id`),
+  CONSTRAINT `Inventory_ibfk_2`
+    FOREIGN KEY (`itemid`)
+    REFERENCES `zbaasdr`.`Item` (`itemid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `zbaasdr`.`Statistic`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `zbaasdr`.`Statistic` (
-  `totalRuns` INT NOT NULL,
-  `totalEnemiesKilled` INT NOT NULL,
-  `totalBossesKilled` INT NOT NULL,
-  `highScore` INT NOT NULL,
-  `Account_user_id` INT NOT NULL,
-  PRIMARY KEY (`Account_user_id`),
-  CONSTRAINT `fk_Statistic_Account1`
-    FOREIGN KEY (`Account_user_id`)
-    REFERENCES `zbaasdr`.`Account` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  `user_id` INT(11) NOT NULL,
+  `totalRuns` INT(11) NULL DEFAULT NULL,
+  `totalEnemiesKilled` INT(11) NULL DEFAULT NULL,
+  `totalBossesKilled` INT(11) NULL DEFAULT NULL,
+  `highScore` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `Statistic_ibfk_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `zbaasdr`.`Account` (`user_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
